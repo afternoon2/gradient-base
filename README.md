0cbf9d76f38b3ea1cedc7f695a3d06eedf510add
Jakub Antolak <poprostuantolak@gmail.com>
2018-07-14 23:21:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# gradient.js
+## Gradient creation library running in the browser 🖌🌈
+
+Gradient maker is a javascript module that takes your source colors array and configuration object, and returns a gradient suitable for your needs. Just choose a mode from `raw`, `css` or `svg`.
+
+Gradient maker uses `chroma-js` color manipulation library for gradient generation (Copyright (c) 2011-2017, Gregor Aisch).
+
+# gradient-base.js
+
+This module creates base gradient output for gradient.js modules.
---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import babel from 'rollup-plugin-babel'
+import cleanup from 'rollup-plugin-cleanup'
+import commonjs from 'rollup-plugin-commonjs'
+import eslint from 'rollup-plugin-eslint'
+import resolve from 'rollup-plugin-node-resolve'
+import eslintConf from './.eslintrc.json'
+import name from './package.json'
+import { terser } from 'rollup-plugin-terser'
+
+const babelConf = {
+    exclude: ['/node_modules/'],
+    plugins: ['external-helpers']
+}
+
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            extend: true,
+            format: 'umd',
+            file: `./dist/${name}.umd.js`,
+            name: 'gradient',
+            external: ['chroma-js'],
+            globals: {
+                'chroma-js': 'chroma'
+            },
+            sourcemap: true
+        }
+    ],
+    watch: {
+        include: 'src/**'
+    },
+    plugins: [
+        resolve(),
+        eslint(eslintConf),
+        babel(babelConf),
+        cleanup({
+            comments: 'none',
+            extensions: '.js'
+        }),
+        commonjs(),
+        terser()
+    ]
+}
